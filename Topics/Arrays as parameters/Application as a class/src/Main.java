440754cf455300ceddbea1c2751e893bd51c0f7f
Dragan Cajic <cajic_dragan@yahoo.com>
2023-01-13 01:35:42 +0100

    solve `Application as a class` problem @ `Arrays as parameters` topic
    ~ Hint ~
    > As simple as it can be! :-P :stuck_out_tongue_winking_eye:
    > P.S. (BUT,) the problem was changed since the last time I've solved it! :thinking:

--- Topics/Arrays as parameters/Application as a class/src/Main.java
+++ Topics/Arrays as parameters/Application as a class/src/Main.java
@@ -2,5 +2,13 @@ class Application {
 
     void run(String[] args) {
         // implement me
+        /*
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+        */
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
